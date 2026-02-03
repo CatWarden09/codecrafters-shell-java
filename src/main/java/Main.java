9c635eed44d5f02da5107b8918fa6f08ab700ca8
CatWarden09 <CatWarden09@proton.me>
2026-02-03 23:58:54 +0400

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,20 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws Exception {
         System.out.print("$ ");
+        Scanner sc = new Scanner(System.in);
+
+        List<String> valid_cmds = new ArrayList<String>();
+
+        String cmd = sc.nextLine();
+
+        if(!valid_cmds.contains(cmd)){
+            System.out.println(cmd + ": " + "command not found");
+        }
+
     }
 }
